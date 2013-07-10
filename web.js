910b43062ca4a90bbb7a28e3a6f9fdaa63bfa4ff
Michael Ryan <michaelvincentryan@gmail.com>
2013-07-10 22:38:13 +0000

    updated to point to index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var file = fs.readFileSync('index.html',encoding);
+  var file = fs.readFileSync('index.html');
   output = buf.toString();
   response.send(output);
 });
